e44a397a76d4da26b015446b7d82a52c2aae3d62
dwicao <lutfiandwica@gmail.com>
2017-01-22 20:26:04 +0700

    add slide menu drawer

--- src/components/index.js
+++ src/components/index.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react';
 import {Scene, Router} from 'react-native-router-flux';
-
+import Drawer from 'react-native-drawer'
 import LoginScreen from './Screens/Login';
 import SignupScreen from './Screens/Signup';
 import MainScreen from './Screens/Main';
 import EditScreen from './Screens/Edit';
+import ControlPanel from './ControlPanel';
 
 export default class App extends Component {
   render() {
     return (
-	    <Router>
-	      <Scene key='root'>
-          <Scene key='loginScreen'
-	        	animation='fade'
-	        	component={LoginScreen}
-	        	hideNavBar={true} initial />
-          <Scene key='signupScreen'
-	        	animation='fade'
-	        	component={SignupScreen}
-	        	hideNavBar={true} />
-	        <Scene key='mainScreen'
-	        	animation='fade'
-	        	component={MainScreen}
-	        	hideNavBar={true} />
-	        <Scene key='editScreen'
-	        	animation='fade'
-	        	hideNavBar={true}
-	        	component={EditScreen} />
-	      </Scene>
-	    </Router>
+      <Drawer
+      type="overlay"
+      content={<ControlPanel />}
+      side='right'
+      tapToClose={true}
+      openDrawerOffset={0.3} // 20% gap on the right side of drawer
+      panCloseMask={0.3}
+      panOpenMask={0.3}
+      closedDrawerOffset={-3}
+      styles={drawerStyles}
+      tweenHandler={(ratio) => ({
+        main: { opacity:(2-ratio)/2 }
+      })}
+      >
+  	    <Router>
+  	      <Scene key='root'>
+            <Scene key='loginScreen'
+  	        	animation='fade'
+  	        	component={LoginScreen}
+              panHandlers={null}
+  	        	hideNavBar={true} initial />
+            <Scene key='signupScreen'
+  	        	animation='fade'
+  	        	component={SignupScreen}
+              panHandlers={null}
+  	        	hideNavBar={true} />
+  	        <Scene key='mainScreen'
+  	        	animation='fade'
+  	        	component={MainScreen}
+              panHandlers={null}
+  	        	hideNavBar={true} />
+  	        <Scene key='editScreen'
+  	        	animation='fade'
+  	        	hideNavBar={true}
+              panHandlers={null}
+  	        	component={EditScreen} />
+  	      </Scene>
+  	    </Router>
+      </Drawer>
     );
   }
+}
+
+const drawerStyles = {
+  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+  main: {paddingRight: 3},
 }
