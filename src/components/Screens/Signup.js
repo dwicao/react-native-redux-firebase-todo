c4c456e9911861bae51357baa90612e022622bcf
dwicao <lutfiandwica@gmail.com>
2017-01-24 20:08:03 +0700

    redirect to Main screen if user logged in at Signup screen

--- src/components/Screens/Signup.js
+++ src/components/Screens/Signup.js
@@ -1,10 +1,28 @@
-import React from 'react';
-
+import React, { Component, PropTypes } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as todoActions from '../../actions/todoActions';
 import Signup from '../LoginSignup/Signup';
+import Main from '../Screens/Main';
 
-const SignupScreen = props => {
-  return (
-    <Signup />
-  );
+class SignupScreen extends Component {
+  render() {
+    if (this.props.userData.email) {
+      return <Main />;
+    }
+
+    return <Signup />;
+  }
 }
-export default SignupScreen;
+
+SignupScreen.propTypes = {
+  userData: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+	return {
+    userData: state.userData,
+	};
+}
+
+export default connect(mapStateToProps)(SignupScreen);
