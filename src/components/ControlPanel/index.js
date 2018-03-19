20224bd26105694cef8dc15bed62558fd9c1394a
dwicao <lutfiandwica@gmail.com>
2018-03-19 07:01:41 +0700

    use prop-types

--- src/components/ControlPanel/index.js
+++ src/components/ControlPanel/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import Dimensions from 'Dimensions';
 import { Actions, ActionConst } from 'react-native-router-flux';
 import {
--- src/components/LoginSignup/Signup.js
+++ src/components/LoginSignup/Signup.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import ButtonSignup from './ButtonSignup';
 import FormSignup from './FormSignup';
 import Logo from './Logo';
